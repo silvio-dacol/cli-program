be406dfca735458f3967375c132f5fcda8f2b6ef
Silvio Da Col <silvio.dacol@outlook.com>
2025-09-30 22:02:49 +0200

    Progress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let config = Config::new(&args);
+
+    println!("\nUsing query {} in file {}\n", config.query, config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{}\n", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
