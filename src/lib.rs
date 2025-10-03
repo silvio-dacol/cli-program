eeca0e09cbd23a018656d63ccf3c8a25dc1af384
Silvio Da Col <silvio.da.col@volvocars.com>
2025-10-03 14:03:42 +0200

    Just wrote the new function

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,10 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    unimplemented!();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
