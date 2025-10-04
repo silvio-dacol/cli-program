ae5f4610c7e2b10d47da42336e83429a5d37f8fa
Silvio Da Col <silvio.dacol@outlook.com>
2025-10-04 17:38:11 +0200

    End of the project

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,17 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!(
-        "\nUsing query {} in file {}\n",
-        config.query, config.file_path
-    );
+    // println!(
+    //     "\nUsing query {} in file {}\n",
+    //     config.query, config.file_path
+    // );
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 
